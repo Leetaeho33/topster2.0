93ca8f4f1af303a2579a62e034a781aebe57e48e
gk <iba3374@naver.com>
2024-01-09 19:20:49 +0900

    refactor: dto에 Builder 추가

--- src/main/java/com/sparta/topster/domain/post/dto/request/PostSortReq.java
+++ src/main/java/com/sparta/topster/domain/post/dto/request/PostSortReq.java
@@ -1,5 +1,6 @@
 package com.sparta.topster.domain.post.dto.request;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
@@ -8,8 +9,9 @@ public class PostSortReq {
     private String sortBy;
     private Boolean asc;
 
+    @Builder
     public PostSortReq(String sortBy, Boolean asc) {
         this.sortBy = sortBy;
-        this.asc = asc == null ? false : true;
+        this.asc = asc == null ? false : asc;
     }
 }
--- src/main/java/com/sparta/topster/domain/post/dto/response/PostListRes.java
+++ src/main/java/com/sparta/topster/domain/post/dto/response/PostListRes.java
@@ -1,8 +1,10 @@
 package com.sparta.topster.domain.post.dto.response;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class PostListRes {
 
     private Long id;
