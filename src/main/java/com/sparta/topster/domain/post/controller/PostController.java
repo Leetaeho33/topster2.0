b4a2fb7d78b72fc296f8a591667a6fa231942730
gk <iba3374@naver.com>
2024-01-09 11:08:42 +0900

    #4 feat: 게시글 컨트롤러 삭제 메소드 추가

--- src/main/java/com/sparta/topster/domain/post/controller/PostController.java
+++ src/main/java/com/sparta/topster/domain/post/controller/PostController.java
@@ -5,12 +5,13 @@ import static org.springframework.http.HttpStatus.CREATED;
 import com.sparta.topster.domain.post.dto.request.PostCreateReq;
 import com.sparta.topster.domain.post.dto.request.PostUpdateReq;
 import com.sparta.topster.domain.post.service.PostService;
-import com.sparta.topster.global.response.RootResponseDto;
+import com.sparta.topster.global.response.RootNoDataRes;
 import com.sparta.topster.global.security.UserDetailsImpl;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,25 +27,38 @@ public class PostController {
     private final PostService postService;
 
     @PostMapping("/topster/{topsterId}/posts")
-    public ResponseEntity<?> create(@Valid @RequestBody PostCreateReq req,
+    public ResponseEntity<RootNoDataRes> create(@Valid @RequestBody PostCreateReq req,
         @PathVariable Long topsterId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
 
         Long postId = postService.save(req, topsterId, userDetails.getUser());
-        RootResponseDto<Object> res = RootResponseDto.builder()
+        RootNoDataRes res = RootNoDataRes.builder()
             .message(postId + "번 게시글 생성 완료하였습니다.")
             .code("201")
             .build();
         return ResponseEntity.status(CREATED).body(res);
     }
 
-    
+
     @PatchMapping("/posts/{id}")
-    public ResponseEntity<?> update(@Valid @RequestBody PostUpdateReq req,
+    public ResponseEntity<RootNoDataRes> update(@Valid @RequestBody PostUpdateReq req,
         @PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails) {
 
         Long postId = postService.update(req, id, userDetails.getUser().getId());
-        RootResponseDto<Object> res = RootResponseDto.builder()
-            .message(postId + "번 게시글 수정 완료하였습니다.")
+        RootNoDataRes res = RootNoDataRes.builder()
+            .message(postId + "번 게시글 생성 완료하였습니다.")
+            .code("201")
+            .build();
+        return ResponseEntity.ok(res);
+    }
+
+
+    @DeleteMapping("/posts/{id}")
+    public ResponseEntity<RootNoDataRes> delete(@PathVariable Long id,
+        @AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+        Long deleteId = postService.delete(id, userDetails.getUser().getId());
+        RootNoDataRes res = RootNoDataRes.builder()
+            .message(deleteId + "번 게시글 생성 완료하였습니다.")
             .code("200")
             .build();
         return ResponseEntity.ok(res);
