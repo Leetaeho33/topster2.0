f4e9fdec604b54b0b9685715779d901430cb06d4
gk <iba3374@naver.com>
2024-01-09 10:33:56 +0900

    #4 feat: 게시글 컨트롤러 게시글 생성 메소드 구현

--- src/main/java/com/sparta/topster/domain/post/controller/PostController.java
+++ src/main/java/com/sparta/topster/domain/post/controller/PostController.java
@@ -1,7 +1,18 @@
 package com.sparta.topster.domain.post.controller;
 
+import static org.springframework.http.HttpStatus.CREATED;
+
+import com.sparta.topster.domain.post.dto.request.PostCreateReq;
 import com.sparta.topster.domain.post.service.PostService;
+import com.sparta.topster.global.response.RootResponseDto;
+import com.sparta.topster.global.security.UserDetailsImpl;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +22,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostController {
 
     private final PostService postService;
+
+    @PostMapping("/topster/{topsterId}/posts")
+    public ResponseEntity<?> create(@Valid @RequestBody PostCreateReq req,
+        @PathVariable Long topsterId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+        Long postId = postService.save(req, topsterId, userDetails.getUser());
+        RootResponseDto<Object> res = RootResponseDto.builder()
+            .message(postId + "번 게시글 생성 완료하였습니다.")
+            .code("200")
+            .build();
+        return ResponseEntity.status(CREATED).body(res);
+    }
 
 
 }
