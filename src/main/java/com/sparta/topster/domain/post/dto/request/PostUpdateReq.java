2549f0ae779b6daf366fa1df94c62e8887e04ca7
gk <iba3374@naver.com>
2024-01-09 10:58:07 +0900

    #4 add: 게시글 수정 요청 DTO 생성

---
+++ src/main/java/com/sparta/topster/domain/post/dto/request/PostUpdateReq.java
@@ -0,0 +1,10 @@
+package com.sparta.topster.domain.post.dto.request;
+
+import jakarta.validation.constraints.Size;
+
+public record PostUpdateReq(
+    @Size(min = 3, max = 50, message = "제목은 3 ~ 50자 입니다.") String title,
+    @Size(min = 10, max = 500, message = "내용은 10 ~ 500자 입니다.") String content
+) {
+
+}
