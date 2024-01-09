d25a3682dba339635e323e8a1b97392760ee167b
gk <iba3374@naver.com>
2024-01-09 10:33:44 +0900

    #4 feat: 게시글 서비스 게시글 생성 메소드 구현

--- src/main/java/com/sparta/topster/domain/post/service/PostService.java
+++ src/main/java/com/sparta/topster/domain/post/service/PostService.java
@@ -1,7 +1,11 @@
 package com.sparta.topster.domain.post.service;
 
+import com.sparta.topster.domain.post.dto.request.PostCreateReq;
+import com.sparta.topster.domain.post.entity.Post;
 import com.sparta.topster.domain.post.repository.PostRepository;
+import com.sparta.topster.domain.topster.entity.Topster;
 import com.sparta.topster.domain.topster.service.TopsterService;
+import com.sparta.topster.domain.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,5 +16,15 @@ public class PostService {
     private final PostRepository postRepository;
     private final TopsterService topsterService;
 
+    public Long save(PostCreateReq req, Long topsterId, User user) {
+        Topster topster = topsterService.getTopster(topsterId);
+        Post post = postRepository.save(Post.builder()
+            .title(req.title())
+            .content(req.content())
+            .topster(topster)
+            .user(user)
+            .build());
+        return post.getId();
+    }
 
 }
