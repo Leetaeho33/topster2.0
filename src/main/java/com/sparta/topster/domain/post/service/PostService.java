98fcb3f60d57e82493c8b36908be5f8a61bf6c1a
gk <iba3374@naver.com>
2024-01-09 12:44:57 +0900

    #4 feat: 게시글 서비스 상세조회 메소드 추가

--- src/main/java/com/sparta/topster/domain/post/service/PostService.java
+++ src/main/java/com/sparta/topster/domain/post/service/PostService.java
@@ -2,6 +2,7 @@ package com.sparta.topster.domain.post.service;
 
 import com.sparta.topster.domain.post.dto.request.PostCreateReq;
 import com.sparta.topster.domain.post.dto.request.PostUpdateReq;
+import com.sparta.topster.domain.post.dto.response.PostDeatilRes;
 import com.sparta.topster.domain.post.entity.Post;
 import com.sparta.topster.domain.post.exception.PostException;
 import com.sparta.topster.domain.post.repository.PostRepository;
@@ -9,6 +10,8 @@ import com.sparta.topster.domain.topster.entity.Topster;
 import com.sparta.topster.domain.topster.service.TopsterService;
 import com.sparta.topster.domain.user.entity.User;
 import com.sparta.topster.global.exception.ServiceException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -35,15 +38,30 @@ public class PostService {
 
 
     public Long update(PostUpdateReq req, Long id, Long userId) {
-        Post post = getUserPost(id, userId);
+        Post post = getUserPost(getPost(id), userId);
         post.update(req.title(), req.content());
         return post.getId();
     }
 
+
     public Long delete(Long id, Long userId) {
-        Post post = getUserPost(id, userId);
+        Post post = getUserPost(getPost(id), userId);
         postRepository.delete(post);
         return id;
+    }
+
+
+    public PostDeatilRes getPostDetail(Long id) {
+        Post post = postRepository.findByIdFetchJoinTopsterAndUser(id)
+            .orElseThrow(() -> new ServiceException(PostException.NOT_FOUND));
+        return PostDeatilRes.builder()
+            .id(post.getId())
+            .title(post.getTitle())
+            .content(post.getContent())
+            .nickname(post.getUser().getNickname())
+            .topsterId(post.getTopster().getId())
+            .createdAt(dateFormat(post.getCreatedAt()))
+            .build();
     }
 
 
@@ -54,11 +72,14 @@ public class PostService {
     }
 
 
-    private Post getUserPost(Long id, Long userId) {
-        Post post = getPost(id);
+    private Post getUserPost(Post post, Long userId) {
         if (post.getUser().getId() != userId) {
             throw new ServiceException(PostException.AccessDeniedError);
         }
         return post;
+    }
+
+    private String dateFormat(LocalDateTime date) {
+        return date.format(DateTimeFormatter.ofPattern("YYYY-MM-dd HH:mm:ss"));
     }
 }
