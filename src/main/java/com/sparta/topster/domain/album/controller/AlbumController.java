6b2970d31b5a7660c037ed21b02ef3741c514678
taeholee <lth1993@naver.com>
2024-01-10 10:23:57 +0900

    #22 ADD. OpenAPI와 Album 도메인 분리를 위해 Album도메인 Controller, Serivce setup

---
+++ src/main/java/com/sparta/topster/domain/album/controller/AlbumController.java
@@ -0,0 +1,22 @@
+package com.sparta.topster.domain.album.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.sparta.topster.domain.album.service.AlbumService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/album")
+public class AlbumController {
+
+    private final AlbumService albumService;
+    @GetMapping("/get/raw-data")
+    public ResponseEntity<Object> getRawData(@RequestParam("artistName") String query) throws JsonProcessingException {
+        return ResponseEntity.ok(albumService.getRawArtistData(query));
+    }
+}
--- src/main/java/com/sparta/topster/domain/album/service/AlbumService.java
+++ src/main/java/com/sparta/topster/domain/album/service/AlbumService.java
@@ -1,4 +1,17 @@
 package com.sparta.topster.domain.album.service;
 
+import com.sparta.topster.domain.album.repository.AlbumRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j(topic = "AlbumService")
+@Service
+@RequiredArgsConstructor
 public class AlbumService {
+    private final AlbumRepository albumRepository;
+
+    public Object getRawArtistData(String query) {
+        return null;
+    }
 }
