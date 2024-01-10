574106bf16d13365da07b745402ea87051867877
taeholee <lth1993@naver.com>
2024-01-10 10:33:57 +0900

    #22 FEAT. AlbumService -> ManiadbService로 rawData 요청

--- src/main/java/com/sparta/topster/domain/album/service/AlbumService.java
+++ src/main/java/com/sparta/topster/domain/album/service/AlbumService.java
@@ -1,6 +1,8 @@
 package com.sparta.topster.domain.album.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.sparta.topster.domain.album.repository.AlbumRepository;
+import com.sparta.topster.domain.maniadb.service.ManiadbService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -10,8 +12,9 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class AlbumService {
     private final AlbumRepository albumRepository;
+    private final ManiadbService maniadbService;
 
-    public Object getRawArtistData(String query) {
-        return null;
+    public Object getRawArtistData(String query) throws JsonProcessingException {
+        return maniadbService.getRawArtistData(query);
     }
 }
