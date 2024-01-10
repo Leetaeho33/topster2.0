fd87af3755af35066f1713d55356db529549c349
taeholee <lth1993@naver.com>
2024-01-10 16:50:48 +0900

    #24 FEAT. TopsterCreateRes 구현

--- src/main/java/com/sparta/topster/domain/topster/dto/res/TopsterCreateRes.java
+++ src/main/java/com/sparta/topster/domain/topster/dto/res/TopsterCreateRes.java
@@ -1,6 +1,21 @@
 package com.sparta.topster.domain.topster.dto.res;
 
+import com.sparta.topster.domain.album.dto.res.AlbumRes;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
 public class TopsterCreateRes {
     String title;
     String content;
+    List<AlbumRes> albumResList;
+
+    @Builder
+    public TopsterCreateRes(String title, String content, List<AlbumRes> albumResList) {
+        this.title = title;
+        this.content = content;
+        this.albumResList = albumResList;
+    }
 }
