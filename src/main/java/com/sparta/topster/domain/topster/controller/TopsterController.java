00266a98e3e0319b15a442e8b383f0d6a5e33833
taeholee <lth1993@naver.com>
2024-01-10 16:50:16 +0900

    #24 FEAT. createTopster 요청 추가

--- src/main/java/com/sparta/topster/domain/topster/controller/TopsterController.java
+++ src/main/java/com/sparta/topster/domain/topster/controller/TopsterController.java
@@ -1,8 +1,16 @@
 package com.sparta.topster.domain.topster.controller;
 
+import com.sparta.topster.domain.topster.dto.req.TopsterCreateReq;
 import com.sparta.topster.domain.topster.service.TopsterService;
+import com.sparta.topster.global.response.RootResponseDto;
+import com.sparta.topster.global.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/api/v1/topsters")
@@ -11,4 +19,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TopsterController {
     private final TopsterService topsterService;
 
+    @PostMapping
+    public ResponseEntity<Object> create(@RequestBody TopsterCreateReq topsterCreateReq,
+                                         @AuthenticationPrincipal UserDetailsImpl userDetails){
+        return ResponseEntity.ok(topsterService.createTopster(topsterCreateReq, userDetails.getUser()));
+    }
 }
