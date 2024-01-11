e372589c063d68e52c54b93bbc07d23ecfbf1765
taeholee <lth1993@naver.com>
2024-01-11 13:55:05 +0900

    #22 REFACTOR. AlbumRes relase -> relaseDate

--- src/main/java/com/sparta/topster/domain/album/dto/res/AlbumRes.java
+++ src/main/java/com/sparta/topster/domain/album/dto/res/AlbumRes.java
@@ -1,19 +1,13 @@
 package com.sparta.topster.domain.album.dto.res;
 
-import com.sparta.topster.domain.song.entity.Song;
 import lombok.Builder;
 import lombok.Getter;
-import org.json.JSONObject;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 public class AlbumRes {
     String title;
     String artist;
-    String release;
+    String releaseDate;
     String image;
 
 
@@ -21,7 +15,7 @@ public class AlbumRes {
     public AlbumRes(String title, String artist, String release, String image) {
         this.title = title;
         this.artist = artist;
-        this.release = release;
+        this.releaseDate = release;
         this.image = image;
     }
 }
