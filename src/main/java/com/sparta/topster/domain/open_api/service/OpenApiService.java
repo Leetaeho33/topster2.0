0bd6074d1a3dbc8371cfeddc4a67255e4a894247
taeholee <lth1993@naver.com>
2024-01-11 13:56:53 +0900

    #14 FIX. OpenApiService interface로 교체

---
+++ src/main/java/com/sparta/topster/domain/open_api/service/OpenApiService.java
@@ -0,0 +1,16 @@
+package com.sparta.topster.domain.open_api.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.sparta.topster.domain.album.entity.Album;
+import com.sparta.topster.domain.song.entity.Song;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.List;
+
+public interface OpenApiService {
+    String getRawArtistData(String query);
+    List<Album> getAlbums(String query);
+//    List<Song> fromJSONToSong(JSONObject item, Album album);
+//    Album fromJSONtoAlbum(JSONObject albumJSON);
+}
