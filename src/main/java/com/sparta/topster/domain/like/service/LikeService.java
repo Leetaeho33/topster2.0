ffdeb79f3302e927a781abf5aacf0a8d7cf9ff1b
JEONGYOUNGGYU <zkdlf90@gmail.com>
2024-01-12 14:51:14 +0900

    #3 FIX : TOPSTER 좋아요 구현

--- src/main/java/com/sparta/topster/domain/like/service/LikeService.java
+++ src/main/java/com/sparta/topster/domain/like/service/LikeService.java
@@ -14,7 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-@Slf4j
+@Slf4j(topic = "LikeService")
 public class LikeService {
 
     private final LikeRepository likeRepository;
@@ -25,33 +25,36 @@ public class LikeService {
     // 해당 id의 탑스터가 존재하는지 검증
     Topster topster = topsterService.getTopster(topsterId);
 
-    // 탑스타 작성자 본인인지 검증
-//    if(topster.getUser().getId().equals(userDetails.getUser().getId())) {
-//      throw new IllegalArgumentException("본인의 게시물에 좋아요를 누를 수 없습니다.");
-//    }
-
-    // 해당 탑스타와 사용자 정보를 가지고 있는 좋아요 객체 생성
-    Like like = new Like(topster, userDetails);
-
     // 해당 탑스타에 대한 모든 좋아요 정보를 조회
-     Optional<Like> optionalLike = likeRepository.findByTopsterIdAndUserId(topsterId, userDetails.getUser()
-        .getId());
+    Like optionalLike = getLike(userDetails.getUser().getId(), topsterId);
 
     log.info("조회된 좋아요 목록을 순회");
       // 현재 사용자가 이미 해당 게시물에 좋아요를 눌렀는지 확인
-      if(optionalLike.isPresent()) {
+      if(optionalLike != null) {
         // 이미 좋아요를 눌렀다면 해당 좋아요 삭제
-        likeRepository.delete(optionalLike.get());
+        likeRepository.delete(optionalLike);
         topster.upAndDownLikeCount(-1);
         return false;
       }
      log.info("좋아요 누르지 않았다면 실행");
     // likeCount + 1
     topster.upAndDownLikeCount(1);
+    // 해당 탑스타와 사용자 정보를 가지고 있는 좋아요 객체 생성
+    Like like = new Like(topster, userDetails);
     // 사용자가 아직 좋아요를 누르지 않았다면 좋아요 정보 추가
     likeRepository.save(like);
 
     return true;
+  }
+
+  public Like getLike(Long userId, Long topsterId) {
+    Optional<Like> optionalLike = likeRepository.findByTopsterIdAndUserId(topsterId, userId);
+    if(optionalLike.isPresent()) {
+      return optionalLike.get();
+    } else {
+      log.info("해당 유저가 탑스터에 좋아요를 누르지 않았습니다.");
+    }
+    return null;
   }
 
 }
