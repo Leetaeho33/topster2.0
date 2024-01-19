c854824d5867165ca9a4dc74c388deef6c643782
JaeHoon <wogns8030@naver.com>
2024-01-19 17:35:23 +0900

    fix : DeleteReq 생성

---
+++ src/main/java/com/sparta/topster/domain/user/dto/deleteDto/DeleteReq.java
@@ -0,0 +1,12 @@
+package com.sparta.topster.domain.user.dto.deleteDto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class DeleteReq {
+
+    String password;
+
+}
