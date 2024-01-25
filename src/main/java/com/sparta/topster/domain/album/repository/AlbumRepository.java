081ff86fbe86bd5f349ce487022c89ff2c7af61a
gk <iba3374@naver.com>
2024-01-25 10:55:27 +0900

    feat: AlbumRepository에 saveAll(), 제목을 통해 리스트를 갖고오는 메소드 추가

--- src/main/java/com/sparta/topster/domain/album/repository/AlbumRepository.java
+++ src/main/java/com/sparta/topster/domain/album/repository/AlbumRepository.java
@@ -1,6 +1,8 @@
 package com.sparta.topster.domain.album.repository;
 
 import com.sparta.topster.domain.album.entity.Album;
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.RepositoryDefinition;
 
 import java.util.Optional;
@@ -11,4 +13,9 @@ public interface AlbumRepository {
     Optional<Album> findById(Long albumId);
     Optional<Album> findByTitle(String albumTitle);
     Album findOrCreateByTitle(String albumTitle);
+
+    List<Album> saveAll(Iterable<Album> albums);
+
+    @Query("select a from Album a where a.title in (:titles)")
+    List<Album> findAllByAlbumTitleList(Iterable<String> titles);
 }
