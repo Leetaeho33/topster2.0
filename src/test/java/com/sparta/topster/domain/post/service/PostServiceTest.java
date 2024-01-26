f46a08847123cdf80e897fc58ed447fe5cf50096
gk <iba3374@naver.com>
2024-01-26 21:05:54 +0900

    test: @ExtendWith, @Mock -> Mockito.mock()으로 변경

--- src/test/java/com/sparta/topster/domain/post/service/PostServiceTest.java
+++ src/test/java/com/sparta/topster/domain/post/service/PostServiceTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 
 import com.sparta.topster.domain.post.dto.request.PostCreateReq;
 import com.sparta.topster.domain.post.dto.request.PostPageReq;
@@ -28,30 +29,25 @@ import java.util.Optional;
 import java.util.concurrent.atomic.AtomicLong;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.test.util.ReflectionTestUtils;
 
-@ExtendWith(MockitoExtension.class)
 class PostServiceTest {
 
-    PostService postService;
     User user;
     User otherUser;
     Topster topster;
 
     Topster otherTopster;
 
-    @Mock
-    TopsterService topsterService;
+    TopsterService topsterService = mock();
+
+    PostService postService = new PostService(new FakePostRepository(), topsterService);
 
     @BeforeEach
     void init() {
-        postService = new PostService(new FakePostRepository(), topsterService);
         user = User.builder()
             .nickname("닉네임")
             .email("abc@naver.com")
