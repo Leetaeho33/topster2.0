ba16cf18faccc06aacf5e52dc0cb4f99ed9751ae
JEONGYOUNGGYU <zkdlf90@gmail.com>
2024-01-29 15:34:18 +0900

    FIX : 생성자 추가

--- src/main/java/com/sparta/topster/domain/comment/dto/res/CommentRes.java
+++ src/main/java/com/sparta/topster/domain/comment/dto/res/CommentRes.java
@@ -2,11 +2,15 @@ package com.sparta.topster.domain.comment.dto.res;
 
 import com.sparta.topster.domain.comment.entity.Comment;
 import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CommentRes {
 
     Long id;
@@ -17,7 +21,7 @@ public class CommentRes {
     public CommentRes(Comment comment) {
         this.id = comment.getId();
         this.content = comment.getContent();
-        this.author = comment.getUser().getUsername();
+        this.author = comment.getUser().getNickname();
         this.createdAt = comment.getCreatedAt();
     }
 }
