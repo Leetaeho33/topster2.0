8dae113642883ebb7165b90d2afae2cb612344c2
taeholee <lth1993@naver.com>
2024-01-29 09:49:57 +0900

    #99 FIX. Page -> Slice로 처리 변경

--- src/main/java/com/sparta/topster/domain/topster/service/TopsterService.java
+++ src/main/java/com/sparta/topster/domain/topster/service/TopsterService.java
@@ -18,10 +18,7 @@ import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -74,11 +71,11 @@ public class TopsterService {
     }
 
 
-    public Page<TopsterGetRes> getTopstersService(Integer pageNum) {
+    public Slice<TopsterGetRes> getTopstersService(Integer pageNum) {
         Pageable pageable = PageRequest.
                 of(pageNum-1,9,
                         Sort.by(Sort.Direction.DESC, "createdAt"));
-        Page<Topster> topsterPage = topsterRepository.findAll(pageable);
+        Slice<Topster> topsterPage = topsterRepository.findAll(pageable);
         if(topsterPage.isEmpty()){
             log.error(NOT_EXIST_TOPSTER.getMessage());
             throw new ServiceException(NOT_EXIST_TOPSTER);
