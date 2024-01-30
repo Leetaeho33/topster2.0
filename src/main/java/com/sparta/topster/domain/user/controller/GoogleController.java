5f92b3432c3d3eb2f85651f8d3c3d451870216d2
JaeHoon <wogns8030@naver.com>
2024-01-30 20:31:27 +0900

    refactor : 반환값 수정

--- src/main/java/com/sparta/topster/domain/user/controller/GoogleController.java
+++ src/main/java/com/sparta/topster/domain/user/controller/GoogleController.java
@@ -1,6 +1,8 @@
 package com.sparta.topster.domain.user.controller;
 
 import com.sparta.topster.domain.user.service.google.GoogleService;
+import com.sparta.topster.global.response.RootNoDataRes;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +20,13 @@ public class GoogleController {
     }
 
     @GetMapping("/{registrationId}")
-    public String googleLogin(@RequestParam String code, @PathVariable String registrationId) {
+    public ResponseEntity<RootNoDataRes> googleLogin(@RequestParam String code, @PathVariable String registrationId) {
         googleService.socialLogin(code, registrationId);
 
-        return "redirect:/";
+        RootNoDataRes res = RootNoDataRes.builder()
+            .code("200")
+            .message("구글 로그인 성공")
+            .build();
+        return ResponseEntity.ok().body(res);
     }
 }
