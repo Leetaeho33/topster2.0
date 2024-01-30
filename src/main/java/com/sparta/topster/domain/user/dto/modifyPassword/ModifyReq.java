56a22a2293fff8e406e29861a51f1d7593784dde
JaeHoon <wogns8030@naver.com>
2024-01-30 11:53:56 +0900

    feat : 비밀번호 변경기능

---
+++ src/main/java/com/sparta/topster/domain/user/dto/modifyPassword/ModifyReq.java
@@ -0,0 +1,15 @@
+package com.sparta.topster.domain.user.dto.modifyPassword;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+@Builder
+@AllArgsConstructor
+public class ModifyReq {
+    String password;
+    String modifyPassword;
+}
