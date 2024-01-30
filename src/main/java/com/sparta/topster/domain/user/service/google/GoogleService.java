340724a18998578811537635879d74b4ed3b4f1f
JaeHoon <wogns8030@naver.com>
2024-01-30 21:14:16 +0900

    refactor : restTemplate -> restClient

--- src/main/java/com/sparta/topster/domain/user/service/google/GoogleService.java
+++ src/main/java/com/sparta/topster/domain/user/service/google/GoogleService.java
@@ -4,24 +4,16 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.sparta.topster.domain.user.entity.User;
 import com.sparta.topster.domain.user.entity.UserRoleEnum;
 import com.sparta.topster.domain.user.repository.UserRepository;
-import com.sparta.topster.global.exception.ErrorCode;
-import com.sparta.topster.global.exception.ServiceException;
-import java.util.Optional;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.RestTemplate;
-
-import static com.sparta.topster.domain.user.excepetion.UserException.DUPLICATE_EMAIL;
+import org.springframework.web.client.RestClient;
 
 @Service
 @RequiredArgsConstructor
@@ -29,8 +21,8 @@ public class GoogleService {
 
     private final Environment env;
     private final PasswordEncoder passwordEncoder;
-    private final RestTemplate restTemplate = new RestTemplate();
     private final UserRepository userRepository;
+    private final RestClient restClient;
 
     public User socialLogin(String code, String registrationId) {
         String accessToken = getAccessToken(code, registrationId);
@@ -40,7 +32,7 @@ public class GoogleService {
         String id = userResourceNode.get("id").asText();
         String email = userResourceNode.get("email").asText();
         String nickname = userResourceNode.get("name").asText();
-        String saveId = id.substring(0,7);
+        String saveId = id.substring(0, 7);
         System.out.println("id = " + saveId);
         System.out.println("email = " + email);
         System.out.println("nickname = " + nickname);
@@ -48,17 +40,16 @@ public class GoogleService {
         String password = UUID.randomUUID().toString();
         String encodedPassword = passwordEncoder.encode(password);
 
-
-        User googleUser = userRepository.findByOAuthId(Long.valueOf(saveId));
+        User googleUser = userRepository.findByGoogleId(Long.valueOf(saveId));
         //OAuthId -> null 회원가입 시작
-        if(googleUser == null) {
+        if (googleUser == null) {
             //email 비교
             User googleEmail = userRepository.findByGoogleEmail(email);
             if (googleEmail != null) {
                 //동일 email 있을 시
                 googleUser = googleEmail;
                 googleUser = googleUser.GoogleIdUpdate(Long.valueOf(saveId));
-            }else{
+            } else {
                 //신규 회원가입
                 googleUser = User.builder()
                     .username(nickname)
@@ -66,7 +57,7 @@ public class GoogleService {
                     .password(encodedPassword)
                     .email(email)
                     .role(UserRoleEnum.USER)
-                    .oauthId(Long.valueOf(saveId))
+                    .googleId(Long.valueOf(saveId))
                     .build();
             }
             return userRepository.save(googleUser);
@@ -87,23 +78,24 @@ public class GoogleService {
         params.add("redirect_uri", redirectUri);
         params.add("grant_type", "authorization_code");
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-
-        HttpEntity entity = new HttpEntity(params, headers);
-
-        ResponseEntity<JsonNode> responseNode = restTemplate.exchange(tokenUri, HttpMethod.POST, entity, JsonNode.class);
+        ResponseEntity<JsonNode> responseNode =
+            restClient.post()
+                .uri(tokenUri)
+                .body(params)
+                .header("Content-Type", MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+                .retrieve()
+                .toEntity(JsonNode.class);
         JsonNode accessTokenNode = responseNode.getBody();
         return accessTokenNode.get("access_token").asText();
     }
 
     private JsonNode getUserResource(String accessToken, String registrationId) {
-        String resourceUri = env.getProperty("oauth2."+registrationId+".resource-uri");
+        String resourceUri = env.getProperty("oauth2." + registrationId + ".resource-uri");
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", "Bearer " + accessToken);
-        HttpEntity entity = new HttpEntity(headers);
-
-        return restTemplate.exchange(resourceUri, HttpMethod.GET, entity, JsonNode.class).getBody();
+        return restClient.get()
+            .uri(resourceUri)
+            .header("Authorization", "Bearer " + accessToken)
+            .retrieve()
+            .toEntity(JsonNode.class).getBody();
     }
 }
